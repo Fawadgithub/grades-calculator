417aaf1d05f7209adcf06254dc03da7f2f81632b
Fawadgithub <fa6843891@gmail.com>
2024-09-14 01:40:38 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+
+var studentscore = prompt("Enter your score");
+
+ if(studentscore >=90 && studentscore <=100){
+alert("The student's grade is" + "\n"
+    + "A+"
+);
+}
+else if(studentscore >=80 && studentscore <=89){
+alert("The student's grade is" + "\n"
+    + "A"
+);
+
+}
+else if(studentscore >=70 && studentscore <=79){
+alert("The student's grade is" + "\n"
+    + "B+"
+);
+
+}
+ else if(studentscore >=60 && studentscore <=69){
+alert("The student's grade is" + "\n"
+    + "B"
+);
+
+ }
+ else if(studentscore >=50 && studentscore <=59){
+alert("The student's grade is" + "\n"
+    + "C"
+);
+
+ }
+ else if(studentscore >=40 && studentscore <=49){
+alert("The student's grade is" + "\n"
+    + "D"
+);
+
+ }
+else{
+alert("The student's grade is" + "\n"
+    + "Fail"
+);
+
+}
